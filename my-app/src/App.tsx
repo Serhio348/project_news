09dab8b5a9f1adcf825a1b750fd0eee871d2ccf5
Serhio348 <serhiosidorovich@gmail.com>
2022-05-17 15:05:43 +0300

    created app-container

---
+++ my-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+import './App.scss';
+import Header from './component/header/Header';
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'newsx.p.rapidapi.com',
+    'X-RapidAPI-Key': '63335a24a2msh3c174c9ace7bed7p12660ajsn0a7b14975146'
+  }
+};
+
+const URL = 'https://newsx.p.rapidapi.com/search?limit=20&skip=0'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="app-container">
+      <Header />
+      <div className="app-content">
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
