b9fb4af0ce9b37bb22ac3b0407b05399f2585d70
Serhio348 <serhiosidorovich@gmail.com>
2022-05-17 18:46:07 +0300

    created store my-app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Header from './component/header/Header';
 
 import './App.scss';
-import Header from './component/header/Header';
+
+
+
 
 
 const options = {
@@ -12,7 +15,20 @@ const options = {
   }
 };
 
-const URL = 'https://newsx.p.rapidapi.com/search?limit=20&skip=0'
+fetch('https://newsx.p.rapidapi.com/search?limit=20&skip=0', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Host': 'newsx.p.rapidapi.com',
+//     'X-RapidAPI-Key': '63335a24a2msh3c174c9ace7bed7p12660ajsn0a7b14975146'
+//   }
+// };
+
+// const URL = 'https://newsx.p.rapidapi.com/search?limit=20&skip=0'
 
 const App: React.FC = () => {
 
--- my-app/src/component/header/Header.tsx
+++ my-app/src/component/header/Header.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { ReactComponent as LogoutIcon } from "../../assets/logout.svg";
+import { ReactComponent as LoginIcon } from "../../assets/login.svg";
+
+import "./Header.scss"
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
     return (
-        <div>
 
-        </div>
+        <nav className="header-container">
+            <div className="logo">
+                <Logo />
+                <div className="app-name">
+                    World News
+                </div>
+            </div>
+
+            <ul className="links">
+                <li>
+                    <a href="/">Country</a>
+                </li>
+                <li>
+                    <a href="/">Category</a>
+                </li>
+                <li>
+                    <a href="/">My news</a>
+                </li>
+
+            </ul>
+            <div className="controls">
+                {logged ?
+                    <>
+                        <LogoutIcon onClick={handleLogout} className="icon-button logout-button" />
+                        <Username />
+                    </>
+                    :
+                    <Link to="../login">
+                        <LoginIcon onClick={handleLogout} className="icon-button logout-button" />
+                    </Link>
+                }
+            </div>
+
+
+        </nav>
+
     );
 };
 
