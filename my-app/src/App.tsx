4ac02515f4d35aed3527447a1dd3ebfb6c3c4c7f
Serhio348 <serhiosidorovich@gmail.com>
2022-05-22 15:56:13 +0300

    bug fixes app_tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,12 +3,14 @@ import Header from './component/header/Header';
 import NewsPosts from './component/newsPosts/NewsPosts';
 
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App: React.FC = () => {
 
   return (
     <div className="app-container">
       <Header />
       <div className="app-content">
+
         <NewsPosts />
       </div>
     </div>
