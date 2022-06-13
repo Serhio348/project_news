72b7a780822962c0d7b120d6b9d3b79d991c948e
Serhio348 <serhiosidorovich@gmail.com>
2022-06-13 12:05:44 +0300

    changed header

--- my-app/src/component/header/Header.tsx
+++ my-app/src/component/header/Header.tsx
@@ -1,33 +1,24 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from '../hooks/useSelector';
+import { ReactComponent as LogoIcon } from "../../assets/logo.svg";
+import { useActions } from '../hooks/useActions';
 import { initialState, NewsFilterReducer } from '../newsPosts/NewsFilterReducer';
 import SearchNewsFilter from '../newsPosts/SearchNewsFilter';
-import Username from './username/Username';
-import { ReactComponent as LogoIcon } from "../../assets/logo.svg";
-import { ReactComponent as LogoutIcon } from "../../assets/logout.svg";
-import { ReactComponent as LoginIcon } from "../../assets/login.svg";
+import SortingNewsFilter from '../newsPosts/SortingNewsFilter';
 
 import "./Header.scss"
-import { useActions } from '../hooks/useActions';
-import { Link } from 'react-router-dom';
 
-const getLinks = (logged: boolean) => ([
+const LINKS = [
     { url: "/newsPosts", text: "News" },
-    { url: "/registration", text: "Registration" },
-    ...(!logged ? [] : [
-        { url: "/blogsPosts", text: "Blogs" },
-    ])
-])
+    { url: "/login", text: "Login" },
+    { url: "/registration", text: "Registration" }
+]
 const Header: React.FC = () => {
-    const [state, dispatch] = useReducer(NewsFilterReducer, initialState);
-    const logged = useSelector(state => state.user.logged);
-    const { removeUser } = useActions();
-    const links = getLinks(logged);
-
-    const handleLogout = () => {
-        removeUser()
-    }
+    const [state, dispatch] = useReducer(NewsFilterReducer, initialState)
+    const { fetchPosts } = useActions()
+    useEffect(() => {
+        fetchPosts(state)
+    }, [state]);
     return (
         <nav className="header-container">
             <div className="logo">
@@ -37,7 +28,7 @@ const Header: React.FC = () => {
                 </div>
             </div>
             <ul className="links">
-                {links.map(({ url, text }) =>
+                {LINKS.map(({ url, text }) =>
                     <li key={url + text}>
                         <NavLink to={url} className={({ isActive }) => isActive ? "_active" : ""}>
                             {text}
@@ -52,21 +43,6 @@ const Header: React.FC = () => {
                         state={state}
                     />
                 </div>
-                {logged ?
-                    <>
-                        <LogoutIcon
-                            className='icon-button'
-                            onClick={handleLogout} />
-                        <Username />
-                    </>
-                    :
-                    <Link to="/login">
-                        <LoginIcon
-                            className='icon-button'
-                            onClick={handleLogout} />
-                        <Username />
-                    </Link>
-                }
             </div>
         </nav >
     );
