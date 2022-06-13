829ac0e9b608452fd916cad3ff61416f0d45198f
Serhio348 <serhiosidorovich@gmail.com>
2022-06-13 23:59:35 +0300

    add function jumping in another social site

---
+++ my-app/src/component/footer/Footer.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import "./Footer.scss";
+
+const Footer: React.FC = () => {
+
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/* Colomn 1 */}
+                    <div className="col-md-3 col-sm-6">
+                        <h4>Contact information</h4>
+                        <ul className="list-contact">
+                            <li>Contacts</li>
+                            <li>About me</li>
+                        </ul>
+                    </div>
+                    {/* Colomn 2 */}
+                    <div className="col-md-3 col-sm-6">
+                        <h4>Social networks</h4>
+                        <ul className="list-social">
+                            <li>
+                                <a href="https://www.facebook.com/" target="_blank">
+                                    <FacebookIcon className='icon-button' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/Serhio348/" target="_blank">
+                                    <GitHubIcon className='icon-button' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/" target="_blank">
+                                    <LinkedInIcon className='icon-button' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/" target="_blank">
+                                    <TwitterIcon className='icon-button' />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                {/* Footer Bottom */}
+                <div className="footer-bottom">
+                    <p className="text-xs-center">
+                        &copy; {new Date().getFullYear()}  Serhio Sidorovich
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
